49461fd2a05c61b8f35ceb064e2995dcbd938c9d
macbook <macbook@MacBook-Air-de-macbook.local>
2026-04-22 12:08:11 +0200

    plus d espace bas hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -33,7 +33,7 @@ export default function Hero() {
     <section
       id="accueil"
       ref={ref}
-      className="relative min-h-screen flex items-center justify-center overflow-hidden"
+      className="relative min-h-screen flex items-center justify-center overflow-hidden pb-64"
       style={{ background: "#070714" }}
       aria-label="Accueil"
     >
