1f0e5caf32b31c8a0224d2015054a1eaa575483a
macbook <macbook@MacBook-Air-de-macbook.local>
2026-04-22 09:11:28 +0200

    mise à jour URL embed Google Maps Nomeny

--- components/About.tsx
+++ components/About.tsx
@@ -147,7 +147,7 @@ export default function About() {
               style={{ boxShadow: "0 0 30px rgba(124,58,237,0.3)" }}
             >
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2636.5!2d6.2333!3d48.8833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4794c0b0b0b0b0b0%3A0x0!2sNomeny%2C+France!5e0!3m2!1sfr!2sfr!4v1"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5272.5!2d6.2268!3d48.8833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4794c0a9c0000001%3A0x40b82c3688b3670!2s54610%20Nomeny!5e0!3m2!1sfr!2sfr!4v1682000000000!5m2!1sfr!2sfr"
                 width="100%"
                 height="100%"
                 style={{ border: 0, display: "block", minHeight: "360px", borderRadius: "12px" }}
