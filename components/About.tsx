20ba4dbb8ca974bec1f68ac6f580d719f685a819
macbook <macbook@MacBook-Air-de-macbook.local>
2026-04-23 17:20:54 +0200

    map plus haute

--- components/About.tsx
+++ components/About.tsx
@@ -201,7 +201,7 @@ export default function About() {
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d41983.54873276361!2d6.191466850901038!3d48.87781421148403!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4794eab5e309a6e5%3A0x40a5fb99a3af5a0!2s54610%20Nomeny!5e1!3m2!1sfr!2sfr!4v1776842191976!5m2!1sfr!2sfr"
                 width="100%"
-                height="380"
+                height="500"
                 style={{ border: 0, display: "block" }}
                 allowFullScreen
                 loading="lazy"
